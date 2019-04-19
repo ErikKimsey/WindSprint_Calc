f8037251667f9314da0e3851fe890db72a590d63
Erik Kimsey <kimseyerik@gmail.com>
2019-04-19 10:24:54 -0400

    Built out prelim of FaceView

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FaceView from './Face/FaceView';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <FaceView />
       </div>
     );
   }
--- src/Face/FaceView.js
+++ src/Face/FaceView.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './faceview.scss';
 
 export default class FaceView extends Component {
   render() {
