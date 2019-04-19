0539e7354c785b8c0c9160416d16968cb86ea0b0
Erik Kimsey <kimseyerik@gmail.com>
2019-04-19 10:17:59 -0400

    Working on FaceView.  Created global styles dir, with app colors included in colors.scss

---
+++ src/Face/FaceView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FaceView extends Component {
+  render() {
+    return (
+      <div className="face-view-container">
+        
+      </div>
+    )
+  }
+}
